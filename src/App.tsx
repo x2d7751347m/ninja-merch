1758951748d2a621db3e0d4c4fde27e1a42c6c89
x2d7751347m <62936128+x2d7751347m@users.noreply.github.com>
2025-01-31 22:57:38 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import type {Component} from 'solid-js';
+import banner from './assets/banner.png'
+import Card from "./components/Card";
+
+const App: Component = () => {
+    return (
+        <div class="container m-auto">
+            <header>
+                <h1>Ninja Merch</h1>
+            </header>
+
+            <img class="rounded-md" src={banner} alt="site banner"/>
+
+            <div class="grid grid-cols-4 gap-10 my-4">
+                <Card/>
+                <Card/>
+                <Card/>
+            </div>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/Card.tsx
@@ -0,0 +1,9 @@
+export default function Card() {
+    return (
+        <div>
+            <h2>Card Component</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et magni nostrum perferendis sit ut vitae!</p>
+            <button class="btn">click me!</button>
+        </div>
+    )
+}
