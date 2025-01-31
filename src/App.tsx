ce2e46a504baf7742c4ac225e8e8a7b993bc6917
x2d7751347m <62936128+x2d7751347m@users.noreply.github.com>
2025-02-01 02:09:35 +0900

    event handlers

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import type {Component} from 'solid-js';
+import {Component, createSignal} from 'solid-js';
 import banner from './assets/banner.png'
 import Card from "./components/Card";
 
 const App: Component = () => {
+    const [darkTheme, setDarkTheme] = createSignal(false)
+
+    function toggleTheme() {
+        setDarkTheme(!darkTheme())
+    }
+
     return (
         <div class="container m-auto">
-            <header>
+            <header
+                class="my-4 p-2 text-xl flex items-center gap-4"
+                classList={{"bg-neutral-900": darkTheme(), "text-white": darkTheme()}}
+            >
+                <span
+                    class="material-symbols-outlined cursor-pointer"
+                    onClick={toggleTheme}
+                >
+                    light_mode
+                </span>
                 <h1>Ninja Merch</h1>
             </header>
 
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,10 @@
-export default function Card(props) {
+import {JSX} from "solid-js/jsx-runtime";
+
+export default function Card(props: {
+    rounded: any;
+    flat: any;
+    children: number | boolean | Node | JSX.ArrayElement | (string & {}) | null | undefined;
+}) {
     return (
         <div
             class="bg-white p-4 text-center"
