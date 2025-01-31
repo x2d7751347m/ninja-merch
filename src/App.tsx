3955158bb78ea5836f06042479893ead42bc5cdf
x2d7751347m <62936128+x2d7751347m@users.noreply.github.com>
2025-02-01 00:55:12 +0900

    using props

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,20 @@ const App: Component = () => {
             <img class="rounded-md" src={banner} alt="site banner"/>
 
             <div class="grid grid-cols-4 gap-10 my-4">
-                <Card/>
-                <Card/>
-                <Card/>
+                {/*<Card title="ninja tee"/>*/}
+                <Card rounded={true} flat={false}>
+                    <h2>Ninja Tee, Black</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et magni nostrum perferendis sit ut
+                        vitae!</p>
+                    <button class="btn">view</button>
+                </Card>
+                <Card rounded={false} flat={true}>
+                    <h2>Ninja Tee, White</h2>
+                    <button class="btn">view</button>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et magni nostrum perferendis sit ut
+                        vitae!</p>
+                    <p>Only $10</p>
+                </Card>
             </div>
         </div>
     );
