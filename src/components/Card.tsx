08fb54385dbfad10fdceca203cacb293c64d5ebb
x2d7751347m <62936128+x2d7751347m@users.noreply.github.com>
2025-01-31 23:58:11 +0900

    fixing tailwindcss

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,6 @@
 export default function Card() {
     return (
-        <div>
+        <div class="bg-white p-4 text-center rounded-md shadow-ms">
             <h2>Card Component</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et magni nostrum perferendis sit ut vitae!</p>
             <button class="btn">click me!</button>
